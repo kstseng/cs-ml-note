f1094e4b1930817b236e49f5ba112dcb6d8865a5
KSTseng <ddd7975@gmail.com>
2019-02-15 10:50:51 +0000

    Updates DSA/ProblemSolvingWithAlgorithmsAndDataStructures/MD/BasicDataStructure/basic_data_structure_stack.md
    Auto commit by GitBook Editor

--- DSA/ProblemSolvingWithAlgorithmsAndDataStructures/CODE/BasicDataStructure/stack.py
+++ DSA/ProblemSolvingWithAlgorithmsAndDataStructures/CODE/BasicDataStructure/stack.py
@@ -1,33 +1,3 @@
-# Basic Data Structure - Stack
-
-## Outline
-
-* [Notes](#notes)
-* [Implementation](#implementation)
-    * [Stack Class](#stack-class)
-    * [Balanced Parentheses (括號匹配)](#balanced-parentheses)
-
-### Notes
-
-1. 概念：後進先出 (Last-In-First-Out, LIFO) ，想像成**托盤**或是**一本本疊在桌上的書**。
-1. 應用：瀏覽器的**返回鍵**。
-1. 基本操作：
-    1. `push(item)`: 將一個新項目 (item) 加入 Stack 的頂部 (放一個新托盤)
-    1. `pop()`: 刪除 (修改) Stack 的頂部項目並回傳最頂項 (拿走一個托盤並取得該托盤的訊息)
-    1. `peek()`: 取得 Stack 頂部項目的訊息
-    1. `isEmpty()`: Stack 是否為空
-    1. `size()`: Stack 的項目數量 (幾個托盤)
-    
-
-### Implementation
-
-[code in github](https://github.com/kstseng/dsa-ml-tool-note/blob/master/DSA/ProblemSolvingWithAlgorithmsAndDataStructures/CODE/BasicDataStructure/stack.py)
-
-#### Stack Class
-
-建立一個 Stack Class
-
-```python
 class Stack:
     def __init__(self):
         ## 初始化一個空的 list 來存放 item
@@ -52,15 +22,6 @@ class Stack:
     def size(self):
         return len(self.items)
 
-```
-
-#### Balanced Parentheses
-
-確定符號是否平衡。
-* simpleParChecker: 符號只有 "("。
-* parChecker: 可匹配多個符號，"([{"。
-
-```python
 def simpleParChecker(symbolString):
     """ 簡單括號匹配
 
@@ -151,4 +112,42 @@ def parChecker(symbolString):
         return True
     else:
         return False
-```
+
+def main():
+
+    ## create Stack and test
+    '''
+    s = Stack()
+
+    print(s.isEmpty())
+    s.push(4)
+    s.push('dog')
+    print(s.peek())
+    s.push(True)
+    print(s.size())
+    print(s.isEmpty())
+    s.push(8.4)
+    print(s.pop())
+    print(s.pop())
+    print(s.size())
+    '''
+
+    ## Simple Balanced Parentheses
+    print('[TEST] Simple Balanced Parentheses')
+    assert simpleParChecker('((()))') == True
+    assert simpleParChecker('((())') == False
+    assert simpleParChecker('(()())') == True
+    assert simpleParChecker('(()(()))') == True
+    print('[INFO] pass')
+
+    ## Balanced Parentheses
+    print('[TEST] Balanced Parentheses')
+    assert parChecker('((()))') == True
+    assert parChecker('((())') == False
+    assert parChecker('(()())') == True
+    assert parChecker('(()(()))') == True
+    print('[INFO] pass')
+    
+
+if __name__ == '__main__':
+    main()
